ef299126838ee718dab9996c3240c39a30f7cc65
AASHISH PANDEY <pandeyashish1605@gmail.com>
2026-02-05 00:06:58 +0530

    Added initial C++ practice files

---
+++ file2.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+    // Initialization; Condition; Increment
+    for (int i = 1; i <= 5; i++) {
+        std::cout << "Iteration: " << i << std::endl;
+    }
+    return 0;
+}
